ef7cdfa637a41468029c74222d5490d67da3431c
Abhishek94Bire <salesagroprollp@gmail.com>
2023-11-20 19:49:39 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config(); // Cinfuring dotenv
+
+const database = require('./configurations/databaseConnect.js'); // Importing database
+
+const roomsRouter = require('./routes/rooms.routes.js');
+const bookingRouter = require('./routes/bookings.routes.js');
+
+const app = express(); // Initialising express app
+app.use(express.json()); // Utilising express.json() method
+
+database();
+
+// For welcome message
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to Hall Booking app!!! roomName-1---to---roomName-8 are alreay created in the database.");
+})
+
+        
+// For task-1
+app.use('/api', roomsRouter);
+
+// For tasks - 2, 3, 4, 5
+app.use('/api', bookingRouter);
+
+
+// Listening to the app
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+
